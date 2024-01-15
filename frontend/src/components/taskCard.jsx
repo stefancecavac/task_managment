e8585d3cc0e8cd74fc56a99e6052de98cdb3700b
stefan <cecavac98@gmail.com>
2024-01-15 20:58:40 +0100

    added failed status

--- frontend/src/components/taskCard.jsx
+++ frontend/src/components/taskCard.jsx
@@ -3,52 +3,64 @@ import { useTaskContext } from "../hooks/useTaskContext";
 import { useUserContext } from "../hooks/useUserContext";
 
 
+
+const TaskCard = ({ task }) => {
+  const { user } = useUserContext()
+  const { dispatch } = useTaskContext()
+
+
+  const handleUpdate = async (updatedStatus) => {
+
+    if (!user) {
+      return;
+    }
+
+    const response = await fetch('http://localhost:4000/api/tasks/' + task._id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user.token}`,
+      },
+      body: JSON.stringify({ task_status: updatedStatus }),
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'UPDATE_TASK', payload: json });
+
+    }
+  }
+
+
+  const isTaskFailed = new Date(task.end_date) < new Date();
+
  
-const TaskCard = ({task}) => {
-    const {user} = useUserContext()
-    const {dispatch} = useTaskContext()
+  if (isTaskFailed && task.task_status !== 'failed') {
+    handleUpdate('failed');
+  }
 
 
-    const handleUpdate = async (updatedStatus) => {
+  const handleComplete = async () => {
+    handleUpdate('completed')
+  }
 
-        if (!user) {
-          return;
-        }
-    
-        const response = await fetch('http://localhost:4000/api/tasks/' + task._id, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${user.token}`,
-          },
-          body: JSON.stringify({ task_status: updatedStatus }),
-        });
-    
-        const json = await response.json();
-    
-        if (response.ok) {
-          dispatch({ type: 'UPDATE_TASK', payload: json });
-        
-        }
-      }
-      
-      const handleComplete = async() => {
-        handleUpdate('completed')
-      }
-    
-     
-    return(
 
-        <div className={`taskcard ${task.task_status ==='completed'  ? 'completed' : ''}`}>
-            <h2>{task.title}</h2>
-            <p>start date: {new Date(task.start_date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
-            <p>end date: {new Date(task.end_date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+  return (
 
-           <p>status: {task.task_status}</p>
-        <button onClick={handleComplete}>Complete</button>
-        
-        </div>
-    )
+    <div className={`taskcard ${task.task_status === 'completed' ? 'completed' : task.task_status === 'failed' ? 'failed' : 'in-progress'}`}>
+
+      <h2>{task.title}</h2>
+
+      <p>start date: {new Date(task.start_date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+
+      <p>end date: {new Date(task.end_date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+
+      <p>status: {task.task_status}</p>
+      <button onClick={handleComplete}>Complete</button>
+
+    </div>
+  )
 }
 
 export default TaskCard
